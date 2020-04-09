b52ec4f93af5cb8e032d0bdb38132f0d304ce0d0
Craig Swanson <cswanson02@me.com>
2020-04-08 23:16:45 -0500

    Update DailyWeatherTests.m
    Imported json file and created three unit tests for the daily weather model.

--- DailyWeatherTests/DailyWeatherTests.m
+++ DailyWeatherTests/DailyWeatherTests.m
@@ -9,6 +9,7 @@
 #import <XCTest/XCTest.h>
 #import "LSIFileHelper.h"
 #import "LSICurrentForecast.h"
+#import "LSIDailyForecast.h"
 
 @interface DailyWeatherTests : XCTestCase
 
@@ -19,6 +20,21 @@
 - (void)testExample {
 
     // TODO: Use LSIFileHelper to load JSON from your test bundle
+    NSData *dailyData = loadFile(@"DailyWeather.json", [DailyWeatherTests class]);
+    
+    NSError *jsonError = nil;
+    NSArray *json = [NSJSONSerialization JSONObjectWithData:dailyData options:0 error:&jsonError];
+    if (jsonError) {
+        NSLog(@"JSON Parsin error: %@:", jsonError);
+    }
+    
+    LSIDailyForecast *dailyForecast = [[LSIDailyForecast alloc] initWithArray:json];
+    XCTAssertNotNil(dailyForecast);
+    XCTAssertEqual(8, dailyForecast.dailies.count);
+    
+    LSICurrentForecast *dayTwo = dailyForecast.dailies[1];
+    XCTAssertEqual(4.44, dayTwo.windSpeed.doubleValue);
+
     
     // TODO: Create Unit Tests for each separate JSON file
 
