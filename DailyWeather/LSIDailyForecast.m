e4ebb4fc6472960332e4f3987e0b8723b1e34d81
Craig Swanson <cswanson02@me.com>
2020-04-09 00:40:06 -0500

    Commit before refactoring
    About to refactor code to transfer most properties to the LSIWeatherForecast model file from LSICurrentForecast model file.

--- DailyWeather/LSIDailyForecast.m
+++ DailyWeather/LSIDailyForecast.m
@@ -11,45 +11,6 @@
 
 @implementation LSIDailyForecast
 
-//- (instancetype)initWithTime:(NSDate *)time
-//                     summary:(NSString *)summary
-//                        icon:(NSString *)icon
-//                 sunriseTime:(NSDate *)sunriseTime
-//                  sunsetTime:(NSDate *)sunsetTime
-//           precipProbability:(NSNumber *)precipProbability
-//             precipIntensity:(NSNumber *)precipIntensity
-//                  precipType:(NSString *)precipType
-//              temperatureLow:(NSNumber *)temperatureLow
-//             temperatureHigh:(NSNumber *)temperatureHigh
-//      apparentTemperatureLow:(NSNumber *)apparentTemperatureLow
-//     apparentTemperatureHigh:(NSNumber *)apparentTemperatureHigh
-//                    humidity:(NSNumber *)humidity
-//                    pressure:(NSNumber *)pressure
-//                   windSpeed:(NSNumber *)windSpeed
-//                 windBearing:(NSNumber *)windBearing
-//                     uvIndex:(NSNumber *)uvIndex {
-//    if (self = [super init]) {
-//        _time = time;
-//        _summary = summary;
-//        _icon = icon;
-//        _sunriseTime = sunriseTime;
-//        _sunsetTime = sunsetTime;
-//        _precipProbability = precipProbability;
-//        _precipIntensity = precipIntensity;
-//        _precipType = precipType;
-//        _temperatureLow = temperatureLow;
-//        _temperatureHigh = temperatureHigh;
-//        _apparentTemperatureLow = apparentTemperatureLow;
-//        _apparentTemperatureHigh = apparentTemperatureHigh;
-//        _humidity = humidity;
-//        _pressure = pressure;
-//        _windSpeed = windSpeed;
-//        _windBearing = windBearing;
-//        _uvIndex = uvIndex;
-//    }
-//    return self;
-//}
-
 - (instancetype)initWithDailies:(NSArray<LSICurrentForecast *> *)dailies {
     self = [super init];
     if (self) {
---
+++ DailyWeather/LSIWeatherForecast.m
@@ -0,0 +1,17 @@
+//
+//  LSIWeatherForecast.m
+//  DailyWeather
+//
+//  Created by Craig Swanson on 4/9/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIWeatherForecast.h"
+
+@class LSICurrentForecast;
+
+@implementation LSIWeatherForecast
+
+
+
+@end
