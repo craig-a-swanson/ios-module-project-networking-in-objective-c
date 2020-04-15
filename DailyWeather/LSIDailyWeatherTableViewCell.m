ae230d58aef9309352e4428d480a0b46baf6324d
Craig Swanson <cswanson02@me.com>
2020-04-15 12:27:09 -0500

    Update LSIDailyWeatherTableViewCell.m

--- DailyWeather/LSIDailyWeatherTableViewCell.m
+++ DailyWeather/LSIDailyWeatherTableViewCell.m
@@ -27,13 +27,10 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    // Initialization code
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 - (void)setDailyForecast:(LSIDailyForcast *)dailyForecast {
